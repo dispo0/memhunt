c0b309b75defefaa836a031675201e8bb36b4fa6
dispo0 <martemyanov.nikita81@gmail.com>
2024-12-15 19:36:44 +0300

    Добавлен server.py в корень проекта

--- server.py
+++ server.py
@@ -1,31 +1,12 @@
-from flask import Flask, request, jsonify
-import telebot
-import os
-
-# Заменить на свой токен
-API_TOKEN = '8120908173:AAE29D0SP5MaYxuOglT1QeUs0c74aYcRnFY'
-bot = telebot.TeleBot(API_TOKEN)
+from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return jsonify({"message": "MemMaster API работает!"})
-
 @app.route('/webhook', methods=['POST'])
 def webhook():
-    json_str = request.get_data().decode('UTF-8')
-    print(f"Received webhook: {json_str}")  # Логируем данные запроса
-    update = telebot.types.Update.de_json(json_str)
-    bot.process_new_updates([update])
-    return '', 200
+    data = request.json  # получаем данные JSON
+    print(data)  # можно для отладки, чтобы увидеть, что пришло
+    return "OK", 200
 
-
-
-
-# Запуск сервера
 if __name__ == '__main__':
-    PORT = int(os.environ.get('PORT', 5000))  # Поддержка переменной PORT на Railway
-    app.run(host='0.0.0.0', port=PORT, debug=True)
-
-
+    app.run(debug=True)
